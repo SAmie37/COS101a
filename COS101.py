c34aa1c8506ecbb27721fb863b94ad3910d5aa9d
SAmie37 <akinyelesamuel014@gmail.com>
2024-11-17 15:13:38 +0100

    Add files via upload

---
+++ COS101.py
@@ -0,0 +1,52 @@
+print('BHU/24/04/09/0123')
+name = input("what is your name ?")
+print('hey' + name)
+print("ok,akinyele_samuel_temidayo.lets solve some math")
+
+print('what is the formula for the area of a rectangle ?')
+input("length*width")
+print("let length be = 20 now pick your width and solve ")
+width = input('enter your width')
+solution = 20*int(width)
+print(solution)
+
+print('what about some physics ?')
+input("sure !")
+print('what is the formula for velocity ?')
+input('displacement/time')
+print("let your time be 5 seconds")
+displacement = input('enter your displacement')
+solution = int(displacement)/5
+print(solution)
+
+print('wow you are good lets find something harder ?')
+input("bring it on !")
+print('what is the formula for kinetic energy ?')
+input('0.5*mass*velocity^2')
+input('velocity = 4,mass = 5')
+print("0.5*5*4 = 10")
+input("and 10^2 is also 10*10 therefore the answer is")
+solution = int(10*10)
+print(solution)
+
+print('find out the formula for the circumference of a circle and volume of a cube')
+input("ok,done. the formulas are:")
+print('for the circumference of a circle, formula = 2*3.142*R')
+input("R being the radius")
+print('2*3.142 = 9.43')
+radius = input('enter your radius')
+solution = 9.43*int(radius)
+print(solution)
+
+print(' and for the volume of a cube ! the formula = A^3')
+input('A^3 also being A*A*A')
+print('when the value of A = 4')
+solution = (4*4*4)
+print(solution)
+
+print('Akinyele_samuel_temidayo_BHU/24/04/09/0123')
+
+
+
+
+
